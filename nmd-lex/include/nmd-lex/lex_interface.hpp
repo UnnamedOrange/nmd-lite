751821c0ced628270ef596b7fcf7676b464aac9e
UnnamedOrange <lycheng1215@sina.com>
2021-08-24 23:22:40 +0800

    定义接口。

--- nmd-lex/include/nmd-lex/lex_interface.hpp
+++ nmd-lex/include/nmd-lex/lex_interface.hpp
@@ -1,12 +1,31 @@
 // Copyright (c) UnnamedOrange. Licensed under the MIT License.
 // See the LICENSE file in the repository root for full license text.
 
+#pragma once
+
+#include <string>
+#include <string_view>
+
 #include <nmd-lex/lex_aux.hpp>
 
 _NMD_BEGIN
 _LEX_BEGIN
 
+class source_interface
+{
+protected:
+	[[nodiscard]] virtual char32_t forward() = 0;
+	virtual void backward() = 0;
+	virtual bool eof() const = 0;
+	virtual void reset_source() = 0;
+};
 
+class lex_interface
+{
+public:
+	[[nodiscard]] virtual std::u32string next() = 0;
+	virtual void reset() = 0;
+};
 
 _LEX_END
 _NMD_END
