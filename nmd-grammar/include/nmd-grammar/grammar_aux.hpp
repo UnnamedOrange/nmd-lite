7e2c9675addb6e08a06b3bddf5b601567eb947a8
UnnamedOrange <lycheng1215@sina.com>
2021-08-25 20:57:06 +0800

    创建 nmd-grammar 项目。

---
+++ nmd-grammar/include/nmd-grammar/grammar_aux.hpp
@@ -0,0 +1,8 @@
+﻿// Copyright (c) UnnamedOrange. Licensed under the MIT License.
+// See the LICENSE file in the repository root for full license text.
+
+#define _NMD_BEGIN namespace nmd {
+#define _NMD_END }
+
+#define _GRAMMAR_BEGIN namespace grammar {
+#define _GRAMMAR_END }
---
+++ nmd-grammar/include/nmd-grammar/grammar_core.hpp
@@ -0,0 +1,14 @@
+﻿// Copyright (c) UnnamedOrange. Licensed under the MIT License.
+// See the LICENSE file in the repository root for full license text.
+
+#pragma once
+
+#include <nmd-grammar/grammar_aux.hpp>
+
+_NMD_BEGIN
+_GRAMMAR_BEGIN
+
+
+
+_GRAMMAR_END
+_NMD_END
